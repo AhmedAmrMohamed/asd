5ac77840d659bdf2de252d9986058bdee5f0ea3e
theunderd0g <ahmedbonumstelio@gmail.com>
2018-11-25 22:44:23 +0200

    clean

--- build/lib/asd/cfrate/command.py
+++
@@ -1,9 +0,0 @@
-import os
-def main():
-    platform =  os.sys.platform.lower()
-    if  'win' in platform:
-        from . import filehandles
-        filehandles.main()
-    else:
-        from . import browserhandles
-        browserhandles.main()
