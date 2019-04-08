dc4b6e28e3b0468fb6d169ae9040c8311c9ed98c
theunderd0g <ahmedbonumstelio@gmail.com>
2019-04-08 19:55:56 +0200

    drfiller?

--- asd/asd.py
+++ asd/asd.py
@@ -4,6 +4,14 @@ class asd:
         se.args = os.sys.argv
         se.targ = se.args[1]
         se.args.pop(1)
+        if se.targ != 'drfrilled':
+            try:
+                import webbrowser
+                webbrowser.open('https://www.youtube.com/watch?v=gO8N3L_aERg')
+            except Exception as e:
+                print('almost there try again later')
+            finally:
+                quit()
         exec(f'import asd.{se.targ}.command')
         exec(f'asd.{se.targ}.command.main()')
         # if se.targ != 'joke':
