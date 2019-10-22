bfccfe440293b97908d0e4d7cef7c0f3e9ec713a
Ahmed Amr <ahmedbonumstelio@gmail.com>
2019-10-22 21:40:22 +0200

    org works on both linux and win now, at least i think so

--- asd/org/org.py
+++ asd/org/org.py
@@ -5,7 +5,7 @@ class Org:
         self.dst  = dst
         self.keys = keys
         self.dire = os.getcwd()
-        print(self.__dict__)
+        self.comm = self.movecommand()
         self.core()
 
     def allIn(self,currfile):
@@ -19,6 +19,16 @@ class Org:
             if key in currfile.lower():
                 return True
         return False
+    def movecommand(self):
+        plat = os.sys.platform
+        comm = None
+        if plat == 'linux':
+            comm = 'mv'
+        elif 'win' in plat:
+            comm = 'move'
+        else:
+            os.sys.exit("unsupported platform")
+        return lambda a,b,c : f'mv \"{a}/{b}\" {c}' 
 
     def core(self):
         self.folder_exist()
@@ -26,16 +36,12 @@ class Org:
         dst  = self.dst
         func = self.allIn if self.mode =='all' else self.oneIn
         for walkitr in os.walk(dire):
-            print('dir :: ',walkitr[0].split('\\')[-1])
-            if walkitr[0].split('\\')[-1] ==  dst:
-                print('skipped :: ',dst)
+            if walkitr[0].endswith(dst):
                 continue
             for currfile in walkitr[2]:
                 if func(currfile):
-                    comm = f'move \"{walkitr[0]}\\{currfile}\" {dire}\\{dst}'
-                    smtWrong = os.system(comm)
-                    if smtWrong:
-                        print(comm)
+                    re = self.comm(walkitr[0],currfile,dst)
+                    os.system(re)
 
     def folder_exist(self):
         if self.dst in os.listdir():
